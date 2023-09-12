1b5b0a0be19cf0b5904010e4b5cfaced4e67484d
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-12 20:53:27 +0300

    Add files via upload

--- Login.jsx
+++ Login.jsx
@@ -2,25 +2,19 @@ import { useState } from "react";
 
 function Login(){
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    //const [username, setUsername] = useState('');
+    //const [password, setPassword] = useState('');
 
+    const [data, setData]  = useState({username: '', password: ''})
     
     function handleFormSubmit(event){
         event.preventDefault();
 
-       
-
-        alert(`Login: ${username} Password: ${password}`);    
+        alert(`Login: ${data.username} Password: ${data.password}`);    
     }
 
-    
-    const handleUsername = (event) => {
-        setUsername(event.target.value);
-    }
-
-    const handlePassword = (event) => {
-        setPassword(event.target.value);
+    function handleInputChange(event, name){
+        setData({...data, [name]: event.target.value})
     }
 
     return(
@@ -28,12 +22,16 @@ function Login(){
         <h1>Login Form</h1>
         <form onSubmit={handleFormSubmit}>
             <label>Username: 
-                <input type="text" name="username" value={username} onChange={handleUsername} />
+                <input type="text"
+                       value={data.username} 
+                       onChange={(event) => handleInputChange(event, 'username')} />
             </label>
             
 
             <label>Login: 
-                <input type="password" name="password" value={password} onChange={handlePassword}/>
+                <input type="password" 
+                       value={data.password} 
+                       onChange={(event) => handleInputChange(event, 'password')}/>
             </label>
 
             <button type="submit">Login</button>
